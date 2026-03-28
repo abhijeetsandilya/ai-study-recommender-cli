3376a8ea188e733932a4320da27725cbda80e422
Abhijeet Sandilya <abhijeetsandilya8@gmail.com>
2026-03-28 21:29:23 +0530

    Update main.py
    Fixed the class and import mismatch again

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.model import studyModel
+from src.model import StudyModel
 from src.recommend import get_recommendation
 
 def main():
@@ -11,9 +11,9 @@ def main():
     print("\n--- AI Study Recommender ---")
 
     try:
-        math = int(input("Enter Math score: "))
-        physics = int(input("Enter Physics score: "))
-        dsa = int(input("Enter DSA score: "))
+        math = int(input("Enter Math score:  "))
+        physics = int(input("Enter Physics score:  "))
+        dsa = int(input("Enter DSA score:  "))
     except ValueError:
         print("Invalid input. Please enter numbers.")
         return
@@ -21,8 +21,8 @@ def main():
     prediction = model.predict(math, physics, dsa)
     recommendation = get_recommendation(prediction)
 
-    print(f"\nPredicted Level: {prediction.upper()}")
-    print(f"Recommendation: {recommendation}")
+    print(f"\nPredicted Level:  {prediction.upper()}")
+    print(f"Recommendation:  {recommendation}")
 
 if __name__ == "__main__":
     main()
