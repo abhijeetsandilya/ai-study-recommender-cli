460f663fba8b7742e8b67383458093be63a3849e
Abhijeet Sandilya <abhijeetsandilya8@gmail.com>
2026-03-28 19:05:57 +0530

    Update recommend.py

--- src/recommend.py
+++ src/recommend.py
@@ -1,1 +1,9 @@
-
+def get_recommendation(label):
+    if label == "weak":
+        return "Focus heavily: Study 2-3 hours/day, revise basics, solve easy problems"
+    elif label == "medium":
+        return "Practice regularly: 1-2 hours/day, focus on problem solving"
+    elif label == "strong":
+        return "Maintain level: Revise and solve advanced problems"
+    else:
+        return "Invalid prediction"
